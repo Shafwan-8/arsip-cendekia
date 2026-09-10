0753fc71fb28ebf23f418727df261afb35ee0374
Shafwan <arul.safwan73@gmail.com>
2026-09-10 17:24:40 +0800

    refactor: remove unused project configuration files

--- app/components/AppBackground.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <!-- Background glow ambient effects -->
-  <div class="fixed inset-0 pointer-events-none overflow-hidden -z-10" aria-hidden="true">
-    <div class="absolute -top-40 -left-40 w-96 h-96 bg-brand-500/15 rounded-full blur-3xl"></div>
-    <div class="absolute top-1/3 -right-40 w-96 h-96 bg-cyan-500/10 rounded-full blur-3xl"></div>
-    <div class="absolute bottom-10 left-1/3 w-80 h-80 bg-emerald-600/10 rounded-full blur-3xl"></div>
-  </div>
-</template>
