548364e1e7c49d4b31291c5862ed26c554d0b39c
Shafwan <arul.safwan73@gmail.com>
2026-09-16 15:01:52 +0800

    style: change from icon to svg

--- app/pages/literatur.vue
+++ app/pages/literatur.vue
@@ -389,7 +389,10 @@ const resetFilter = () => {
                     {{ work.display_name || work.title || 'Tanpa Judul' }}
                   </div>
                   <div class="text-[10px] text-slate-500 mt-1 truncate">
-                    ✍️ {{ work.authorships?.map(a => a.author.display_name).slice(0, 3).join(', ') || 'Penulis Anonim' }}
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-3 inline mr-1">
+                      <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
+                    </svg>
+                    {{ work.authorships?.map(a => a.author.display_name).slice(0, 3).join(', ') || 'Penulis Anonim' }}
                   </div>
                 </td>
 
